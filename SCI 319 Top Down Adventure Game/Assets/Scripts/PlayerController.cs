efa97892a576bad6de58d2a11d819fa1273247a4
Vortex111903 <03darianacosta@gmail.com>
2024-10-29 16:42:31 -0400

    Player Script made

---
+++ SCI 319 Top Down Adventure Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private SpriteRenderer sr;
+    
+    public Sprite upSprite;
+    public Sprite leftSprite;
+    public Sprite rightSprite;
+    public Sprite frontSprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = transform.position;
+
+        if (Input.GetKey("w")) 
+        {
+            newPosition.y += speed;
+            //sr.sprite = upSprite;
+        }
+        if (Input.GetKey("a"))
+        {
+            newPosition.x -= speed;
+            //sr.sprite = leftSprite;
+        }
+        if (Input.GetKey("s"))
+        {
+            newPosition.y -= speed;
+            //sr.sprite = frontSprite;
+        }
+        if (Input.GetKey("d"))
+        {
+            newPosition.x += speed;
+            //sr.sprite = rightSprite;
+        }
+        transform.position = newPosition;
+    }
+    private void OnCollisionEnter2D(Collision2D collision) 
+    {
+        
+    }
+}
+
+
